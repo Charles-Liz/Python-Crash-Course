2a906978ada2568dfcc94a1bb9c62c67cb12bae5
121058642@qq.com <Lizhao8023>
2020-09-22 22:28:49 +0800

    。。。

--- Chapter15/15-1 cube.py
+++ Chapter15/15-1 cube.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
-x_values = list(range(1,5001))
+x_values = list(range(1,5001))#list(range(1,5001))
 y_values = [x**3 for x in x_values]
 plt.rcParams['font.sans-serif']='SimHei'
 plt.scatter(x_values,y_values,s=40,edgecolors='none',c=y_values,cmap=plt.cm.Blues)
